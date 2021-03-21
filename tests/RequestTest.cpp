549e18e0fa4006915fd30acffb7812d6660c75ca
Juan Rafael García Blanco <juanrgar@gmail.com>
2021-03-21 12:14:30 +0100

    Add RequestTest

--- tests/RequestTest.cpp
+++ tests/RequestTest.cpp
@@ -37,7 +37,7 @@ RequestTest::testReturnVoid(void)
     Smp::AnySimple ret = r->GetReturnValue();
     CPPUNIT_FAIL("Exception not triggered");
   } catch (Smp::IRequest::VoidOperation& e) {
-    CPPUNIT_ASSERT(strcmp(e.GetName(), "SampleVoidMethod") == 0);
+    CPPUNIT_ASSERT(strcmp(e.GetDescription(), "Operation 'SampleVoidMethod' is a void operation") == 0);
   }
   delete r;
 }
@@ -55,11 +55,12 @@ RequestTest::testReturnType(void)
   }
   Smp::Mdk::AnySimple retValue;
   retValue.Set(Smp::Int32(24));
+  r->SetReturnValue(retValue);
   try {
     Smp::AnySimple ret = r->GetReturnValue();
     CPPUNIT_ASSERT(ret.type == Smp::ST_Int32);
     CPPUNIT_ASSERT(ret.type == retValue.type);
-    CPPUNIT_ASSERT(ret.value.int32Value == retValue.value.int32Value == 24);
+    CPPUNIT_ASSERT(ret.value.int32Value == retValue.value.int32Value);
   } catch (Smp::IRequest::VoidOperation& e) {
     CPPUNIT_FAIL("Exception triggered");
   }
--- tests/TestRunner.cpp
+++ tests/TestRunner.cpp
@@ -1,3 +1,21 @@
+/** This file is part of smp-mdk
+ *
+ * Copyright (C) 2021 Juan R. Garcia Blanco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cppunit/ui/text/TestRunner.h>
 
 #include "ArrayTest.h"
@@ -10,6 +28,7 @@
 #include "ModelTest.h"
 #include "ObjectTest.h"
 #include "ReferenceTest.h"
+#include "RequestTest.h"
 #include "StringTest.h"
 #include "UuidTest.h"
 
@@ -28,6 +47,7 @@ main(int argc, char* argv[])
   runner.addTest(ReferenceTest::suite());
   runner.addTest(ContainerTest::suite());
   runner.addTest(CompositeTest::suite());
+  runner.addTest(RequestTest::suite());
   runner.addTest(ManagedContainerTest::suite());
   runner.addTest(ManagedReferenceTest::suite());
   bool testResult = runner.run();
